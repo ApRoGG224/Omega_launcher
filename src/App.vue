727346bc82f5e7a7d7477ca44a8c788a53f8a33d
ApRoGG <bogdankobzak89@gmail.com>
2026-03-12 23:20:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ListMods from './components/ListMods.vue'
+</script>
+
+<template>
+  <div class="app-container">
+    <h3>Omega Launcher</h3>
+    <ListMods />
+  </div>
+</template>
+
+<style scoped>
+h3 {
+  font-weight: lighter;
+  color: rgba(243, 62, 92, 0.763);
+  font-size: 2.5rem;
+}
+</style>
---
+++ src/components/ListMods.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="list-mods">
+    <p>List of mods will be here.</p>
+  </div>
+</template>
+
+<style scoped>
+.list-mods {
+  padding: 1rem;
+  /*background: rgba(255, 255, 255, 0.05);*/
+  /*border-radius: 8px;*/
+}
+</style>
